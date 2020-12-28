91d44c9a4063ea35804c61c2d6687ecdbc817211
Yuki Mizuta <nguen1225@gmail.com>
2020-12-28 21:38:59 +0900

    footer作成バグ有り

--- lib/footer.dart
+++ lib/footer.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class Footer extends StatefulWidget {
@@ -7,20 +9,100 @@ class Footer extends StatefulWidget {
   _Footer createState() => _Footer();
 }
 
-class _Footer extends State {
+// class _Footer extends State {
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationBar(
+//       items: const [
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.home),
+//           title: Text('home'),
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(Icons.home),
+//           title: Text('Home'),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+
+class _Footer extends State<Footer> {
+  //アイコン情報
+  static const _footerIcons = [
+    Icons.home,
+    Icons.textsms
+    Icons.access_time,
+    Icons.content_paste,
+    Icons.work,
+
+  ];
+
+  //アイコンの文字列
+  static const _footerItemNames = [
+    'ホーム'
+    'トーク'
+    'タイムライン'
+    'ニュース'
+    'ウォレット'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _bottomNavigationBarItems.add(_UpdateActiveState(0));
+    for ( var i = 1; i < _footerItemNames.length; i++) {
+      _bottomNavigationBarItems.add(_UpdaateDeactiveState(i));
+    }
+  }
+
+  //indexのアイテムをアクティベート
+  BottomNavigationBarItem _UpdateActiveState(init index) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        _footerIcons[index],
+        color: Colors.black87,
+      ),
+      title: Text(
+        _footerItemNames[index],
+        style: TextStyle(
+          color: Colors.black87,
+        ),
+      )
+    );
+  }
+  //indexのアイテムをディアクティブ
+  BottomNavigationBarItem _UpdateDeactiveState(init index) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        _footerIcons[index],
+        color: Colors.black26,
+      ),
+      title: Text(
+        _footerItemNames[index],
+        style: TextStyle(
+          color: Colors.black26,
+        ),
+      )
+    );
+  }
+
+  Void _onItemTapped(int index) {
+    setState(() {
+      _bottomNavigationBarItems[_selectedIndex] = _UpdateDeactiveState(_selectedIndex);
+      _bottomNavigationBarItems[index] = _UpdateActiveState(index);
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('home'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Home'),
-        ),
-      ],
+      type: BottomNavigationBarType.fixed,
+      items: _bottomNavigationBarItems,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
     );
   }
 }
