d72c6cc1d07fca0f5a0cfb79be703faf3dd3efd6
Yuki Mizuta <nguen1225@gmail.com>
2020-12-30 19:49:00 +0900

    トーク一覧作成

--- lib/routes/talk_route.dart
+++ lib/routes/talk_route.dart
@@ -9,22 +9,37 @@ class Talk extends StatelessWidget {
         title: Text("トーク"),
       ),
       body: ListView(padding: const EdgeInsets.all(8), children: <Widget>[
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
-        Tile(),
+        Tile(
+          icon: Icons.person,
+          username: "鹿太郎",
+          message: "しかし、鹿しかいない",
+        ),
+        Tile(
+          icon: Icons.person,
+          username: "久米酒",
+          message: "おいしいよー",
+        ),
+        Tile(icon: Icons.person, username: "くら", message: "とっても美味しい沖縄のお酒"),
+        Tile(icon: Icons.person, username: "団長", message: "止まるんじゃ、ねぇぞ"),
+        Tile(icon: Icons.person, username: "サルーイン", message: "こい"),
+        Tile(
+            icon: Icons.person,
+            username: "がらはど",
+            message: "だめだ！いくら積まれても..."),
+        Tile(
+            icon: Icons.person,
+            username: "太郎",
+            message: "だめだ、久しぶりにキレちまったよ"),
+        Tile(
+            icon: Icons.person,
+            username: "Harry",
+            message: "エクスペクト・パトローナーーム"),
+        Tile(
+            icon: Icons.person,
+            username: "くろひげ",
+            message: "似合ってるぜぃ、そのきずぅ〜"),
+        Tile(icon: Icons.person, username: "あすらん", message: "キラァァァァ"),
+        Tile(icon: Icons.person, username: "知人B", message: "1.14 release !"),
       ]),
     );
   }
